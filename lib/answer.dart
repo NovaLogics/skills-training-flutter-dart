e55450d0d90db282d5daa4e3f535161d9530223e
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 17:51:10 +0530

    feat [ui]: Update main ui with dynamic actions

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
+  final String answerText;
 
-  const Answer(this.selectHandler);
+  const Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,7 @@ class Answer extends StatelessWidget {
       margin: const EdgeInsets.all(16.0),
       padding: const EdgeInsets.all(2.0),
       child: ElevatedButton(
-        child: Text("Answer 1"),
+        child: Text(answerText),
         onPressed: selectHandler,
         style: ElevatedButton.styleFrom(
           primary: Colors.teal,
--- lib/main.dart
+++ lib/main.dart
@@ -48,10 +48,11 @@ class _AlphaAppState extends State<AlphaApp> {
             Question(
                 questions[_questionIndex]["questionText"] as String // ?? ''
                 ),
-            (questions[_questionIndex]["answers"] as List<String>)
-                .map((answer) {
-              return Answer(answer);
-            }),
+            ...(questions[_questionIndex]["answers"] as List<String>).map(
+              (answer) {
+                return Answer(_answerQuestion, answer);
+              },
+            ).toList(),
           ],
         ),
       ),
