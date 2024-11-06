89af18434cedf48b3044d2c2ba19d0eaa9f23447
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 16:32:06 +0530

    feat [ui]: Updated Question widget

--- lib/question.dart
+++ lib/question.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
+
 class Question extends StatelessWidget {
-  const Question({ Key? key }) : super(key: key);
+  String questionText;
+
+  Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Text(questionText);
   }
 }
