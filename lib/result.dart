21ac3c8e609f3bcf0b5b7ef404aa0142bc749b0f
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 19:33:05 +0530

    feat [ui]: Add Score result showing functions

--- lib/result.dart
+++ lib/result.dart
@@ -5,10 +5,34 @@ class Result extends StatelessWidget {
 
   const Result(this.resultScore);
 
+  String get resultPhrase {
+    String resultText;
+    if(resultScore <= 8){
+      resultText = "You are awesome and innocent!";
+    }
+    else if(resultScore <= 12){
+      resultText = "Pretty Likable!";
+    }
+     else if(resultScore <= 16){
+      resultText = "You are ... Strange?!";
+    }
+    else{
+      resultText = "You are a ... very Strange person?!";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("You did it!"),
+    return Center(
+      child: Text(
+        resultPhrase,
+        style: const TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
