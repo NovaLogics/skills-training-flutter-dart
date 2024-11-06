ce892cd68fd0fb41eed50acc3994cb85762b2f76
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 20:33:37 +0530

    feat [ui]: Stage 3 - Update Answer UI style

--- lib/answer.dart
+++ lib/answer.dart
@@ -10,15 +10,19 @@ class Answer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+      margin: const EdgeInsets.fromLTRB(24.0, 12.0, 24.0, 12.0),
       padding: const EdgeInsets.all(2.0),
       child: ElevatedButton(
         child: Text(answerText),
         onPressed: selectHandler,
         style: ElevatedButton.styleFrom(
-          primary: Colors.teal,
-          onPrimary: Colors.white,
-        ),
+            primary: Colors.teal[600],
+            onPrimary: Colors.white,
+            elevation: 4.0,
+            padding: const EdgeInsets.all(14.0),
+            textStyle: const TextStyle(
+              fontSize: 16.0,
+            )),
       ),
     );
   }
