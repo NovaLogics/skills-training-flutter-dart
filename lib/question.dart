e12173414d0b3ef8bb360c8db175b83a47cd5022
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 15:56:55 +0530

    feat [ui]: Add Question widget

---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+class Question extends StatelessWidget {
+  const Question({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
