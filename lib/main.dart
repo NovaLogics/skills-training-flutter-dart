046584117fdce27ea4ce18b810b840e8a3e8f2ac
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 17:44:51 +0530

    feat [ui]: Update Answers widget to dynamically generate

--- lib/main.dart
+++ lib/main.dart
@@ -45,11 +45,13 @@ class _AlphaAppState extends State<AlphaApp> {
         ),
         body: Column(
           children: <Widget>[
-            Question(questions.elementAt(_questionIndex)),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            Question(
+                questions[_questionIndex]["questionText"] as String // ?? ''
+                ),
+            (questions[_questionIndex]["answers"] as List<String>)
+                .map((answer) {
+              return Answer(answer);
+            }),
           ],
         ),
       ),
