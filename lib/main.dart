7e00ac3dffa888757ab5f118e639322aa5c1801f
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 15:02:06 +0530

    feat [ui]: Add buttons and header text

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,35 @@ void main() => runApp(AlphaApp());
 class AlphaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite colour?",
+      "What's your favorite Animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Flutter Alpha App"),
         ),
         body: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Text("Question"),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Answer 1"),
+            ),
+             ElevatedButton(
+              onPressed: () {},
+              child: Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Answer 3"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Answer 4"),
+            ),
+          ],
         ),
       ),
     );
