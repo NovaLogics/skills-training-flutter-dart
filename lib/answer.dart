a925cc7db6ff5cbf80042b5885306238f69b92aa
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-06 17:10:05 +0530

    feat [ui]: Update Answer widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(2.0),
+      child: ElevatedButton(
+        child: Text("Answer 1"),
+        onPressed: null,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.teal,
+          onPrimary: Colors.white, 
+        ),
+      ),
+    );
   }
 }
